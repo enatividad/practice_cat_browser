622453c1e45432c8242e40b855eebb1f1da76660
Emmanuel Natividad <emmanuel@natividad.info>
2021-06-15 10:33:18 +0800

    + use theCatApi to populate CatPage info

--- src/pages/CatPage.js
+++ src/pages/CatPage.js
@@ -1,4 +1,13 @@
 import {
+  useEffect,
+  useState,
+} from 'react';
+
+import theCatApi from '../services/theCatApi';
+
+import {
+  useParams,
+
   Link,
 } from 'react-router-dom';
 
@@ -6,25 +15,42 @@ import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 
 export default function CatPage() {
-  return (
+  const catId = useParams().catId;
+  const [image, setImage] = useState();
+  useEffect(() => {
+    if (catId) {
+      theCatApi(`images/${catId}`)
+        .then(res => res.json())
+        .then(i => setImage(i || null));
+    } else {
+      setImage(null);
+    }
+  }, [catId]);
+
+  const breed = image?.breeds?.[0];
+
+  return typeof image === 'undefined' ? (
+    <Container>
+      <h5>Loading…</h5>
+    </Container>
+  ) : image && breed ? (
     <Container>
       <Card>
         <Card.Header>
-          <Link className="btn btn-primary" to="/cats?breed=abys">
+          <Link className="btn btn-primary" to={`/cats?breed=${breed.id}`}>
             Back
           </Link>
         </Card.Header>
-        <Card.Img variant="top" src="https://cdn2.thecatapi.com/images/xnzzM6MBI.jpg" />
+        <Card.Img variant="top" src={image.url} />
         <Card.Body>
-          <h4>Abyssinian</h4>
-          <h5>Origin: Egypt</h5>
-          <h6>Active, Energetic, Independent, Intelligent, Gentle</h6>
-          <p>
-            The Abyssinian is easy to care for, and a joy to have in your home.
-            They’re affectionate cats and love both people and other animals.
-          </p>
+          <h4>{breed.name}</h4>
+          <h5>Origin: {breed.origin}</h5>
+          <h6>{breed.temperament}</h6>
+          <p>{breed.description}</p>
         </Card.Body>
       </Card>
     </Container>
+  ) : (
+    null
   );
 }
